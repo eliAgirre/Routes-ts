7d6aa90221f66435284958cee39fbe446b7ca4bb
unknown <eli91ai@gmail.com>
2020-03-20 20:32:11 +0100

    routes ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes : Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'hello/:name', component: HelloComponent},
+  {path: '**', component: ErrorComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorComponent,
+    HelloComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
